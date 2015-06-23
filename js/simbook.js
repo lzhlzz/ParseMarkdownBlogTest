b760151d0360ac32990b47640e89f042b6ad218a
Zhihua Lv <phantomtyry@126.com>
2015-06-23 11:18:38 +0900

    update comment box

--- js/simbook.js
+++ js/simbook.js
@@ -66,6 +66,14 @@ simbook.updateContent = function(){
             hljs.highlightBlock(block);
         });
 
+        var disqus_shortname = content_file.replace("./","");
+    
+        (function() {
+            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+             dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        })();
+
     });
 }
 
