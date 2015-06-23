839c0d03f4115a8c67d105ef546e584c0a296481
Geoff Lui <phantomtyry@126.com>
2015-06-23 11:22:00 +0900

    Update simbook.js

--- js/simbook.js
+++ js/simbook.js
@@ -66,14 +66,6 @@ simbook.updateContent = function(){
             hljs.highlightBlock(block);
         });
 
-        var disqus_shortname = simbook.content_file.replace("./","");
-    
-        (function() {
-            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-             dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-        })();
-
     });
 }
 
